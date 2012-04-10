62a980f1c9ad34a01299883be226aa2ed4261c83
Themaister <maister@archlinux.us>
2012-04-10 20:31:45 +0200

    Update for libretro.

--- libgambatte/libsnes/libretro.cpp
+++ libgambatte/libsnes/libretro.cpp
@@ -1,4 +1,4 @@
-#include <libsnes.hpp>
+#include "libretro.h"
 #include "resamplerinfo.h"
 
 #define HAVE_STDINT_H
@@ -7,25 +7,25 @@
 #include <assert.h>
 #include <stdio.h>
 
-static snes_audio_sample_t audio_cb;
-static snes_video_refresh_t video_cb;
-static snes_input_poll_t input_poll_cb;
-static snes_input_state_t input_state_cb;
-static snes_audio_sample_batch_t audio_batch_cb;
+static retro_video_refresh_t video_cb;
+static retro_input_poll_t input_poll_cb;
+static retro_input_state_t input_state_cb;
+static retro_audio_sample_batch_t audio_batch_cb;
+static retro_environment_t environ_cb;
 static gambatte::GB gb;
 
 namespace input
 {
    struct map { unsigned snes; unsigned gb; };
    static const map btn_map[] = {
-      { SNES_DEVICE_ID_JOYPAD_A, gambatte::InputGetter::A },
-      { SNES_DEVICE_ID_JOYPAD_B, gambatte::InputGetter::B },
-      { SNES_DEVICE_ID_JOYPAD_SELECT, gambatte::InputGetter::SELECT },
-      { SNES_DEVICE_ID_JOYPAD_START, gambatte::InputGetter::START },
-      { SNES_DEVICE_ID_JOYPAD_RIGHT, gambatte::InputGetter::RIGHT },
-      { SNES_DEVICE_ID_JOYPAD_LEFT, gambatte::InputGetter::LEFT },
-      { SNES_DEVICE_ID_JOYPAD_UP, gambatte::InputGetter::UP },
-      { SNES_DEVICE_ID_JOYPAD_DOWN, gambatte::InputGetter::DOWN },
+      { RETRO_DEVICE_ID_JOYPAD_A, gambatte::InputGetter::A },
+      { RETRO_DEVICE_ID_JOYPAD_B, gambatte::InputGetter::B },
+      { RETRO_DEVICE_ID_JOYPAD_SELECT, gambatte::InputGetter::SELECT },
+      { RETRO_DEVICE_ID_JOYPAD_START, gambatte::InputGetter::START },
+      { RETRO_DEVICE_ID_JOYPAD_RIGHT, gambatte::InputGetter::RIGHT },
+      { RETRO_DEVICE_ID_JOYPAD_LEFT, gambatte::InputGetter::LEFT },
+      { RETRO_DEVICE_ID_JOYPAD_UP, gambatte::InputGetter::UP },
+      { RETRO_DEVICE_ID_JOYPAD_DOWN, gambatte::InputGetter::DOWN },
    };
 }
 
@@ -37,30 +37,34 @@ class SNESInput : public gambatte::InputGetter
          input_poll_cb();
          unsigned res = 0;
          for (unsigned i = 0; i < sizeof(input::btn_map) / sizeof(input::map); i++)
-         {
-            res |= input_state_cb(SNES_PORT_1,
-                  SNES_DEVICE_JOYPAD, 0, input::btn_map[i].snes) ? input::btn_map[i].gb : 0;
-         }
+            res |= input_state_cb(0, RETRO_DEVICE_JOYPAD, 0, input::btn_map[i].snes) ? input::btn_map[i].gb : 0;
          return res;
       }
 } static gb_input;
 
 static Resampler *resampler;
 
-// SSNES extension.
-static snes_environment_t environ_cb;
-static bool can_dupe = false;
-void snes_set_environment(snes_environment_t cb)
+
+void retro_get_system_info(struct retro_system_info *info)
 {
-   bool dummy;
-   environ_cb = cb;
-   dummy = 0;
-   cb(SNES_ENVIRONMENT_SET_BATCH_LOAD, &dummy);
-   cb(SNES_ENVIRONMENT_SET_ROM_FORMATS, (void*)"gb|gbc|dmg|zip|GB|GBC|DMG|ZIP");
-   cb(SNES_ENVIRONMENT_SET_CORE_VERSION, (void*)"0.5.0");
+   info->library_name = "gambatte";
+   info->library_version = "v0.5.0";
+   info->need_fullpath = false;
+   info->block_extract = false;
+   info->valid_extensions = "gb|gbc|dmg|zip|GB|GBC|DMG|ZIP";
 }
 
-void snes_init()
+static bool can_dupe = false;
+static struct retro_system_timing g_timing;
+
+void retro_get_system_av_info(struct retro_system_av_info *info)
+{
+   retro_game_geometry geom = { 160, 144, 160, 144 };
+   info->geometry = geom;
+   info->timing   = g_timing;
+}
+
+void retro_init()
 {
    // Using uint_least32_t in an audio interface expecting you to cast to short*? :( Weird stuff.
    assert(sizeof(gambatte::uint_least32_t) == sizeof(uint32_t));
@@ -73,51 +77,45 @@ void snes_init()
 
    if (environ_cb)
    {
-      snes_geometry geom = { 160, 144, 160, 144 };
-      environ_cb(SNES_ENVIRONMENT_SET_GEOMETRY, &geom);
-
-      snes_system_timing timing;
-      timing.fps = fps;
+      g_timing.fps = fps;
 
       unsigned long mul, div;
       resampler->exactRatio(mul, div);
 
-      timing.sample_rate = sample_rate * mul / div;
-      environ_cb(SNES_ENVIRONMENT_SET_TIMING, &timing);
-      environ_cb(SNES_ENVIRONMENT_GET_AUDIO_BATCH_CB, &audio_batch_cb);
+      g_timing.sample_rate = sample_rate * mul / div;
 
-      environ_cb(SNES_ENVIRONMENT_GET_CAN_DUPE, &can_dupe);
+      environ_cb(RETRO_ENVIRONMENT_GET_CAN_DUPE, &can_dupe);
       if (can_dupe)
          fprintf(stderr, "[Gambatte]: Will dupe frames with NULL!\n");
    }
 }
 
-void snes_term()
+void retro_deinit()
 {
    delete resampler;
 }
 
-void snes_set_video_refresh(snes_video_refresh_t cb) { video_cb = cb; }
-void snes_set_audio_sample(snes_audio_sample_t cb) { audio_cb = cb; }
-void snes_set_input_poll(snes_input_poll_t cb) { input_poll_cb = cb; }
-void snes_set_input_state(snes_input_state_t cb) { input_state_cb = cb; }
+void retro_set_environment(retro_environment_t cb) { environ_cb = cb; }
+void retro_set_video_refresh(retro_video_refresh_t cb) { video_cb = cb; }
+void retro_set_audio_sample(retro_audio_sample_t) {}
+void retro_set_audio_sample_batch(retro_audio_sample_batch_t cb) { audio_batch_cb = cb; }
+void retro_set_input_poll(retro_input_poll_t cb) { input_poll_cb = cb; }
+void retro_set_input_state(retro_input_state_t cb) { input_state_cb = cb; }
 
-void snes_set_controller_port_device(bool, unsigned) {}
-void snes_set_cartridge_basename(const char *) {}
+void retro_set_controller_port_device(unsigned, unsigned) {}
 
-void snes_power() { gb.reset(); }
-void snes_reset() { gb.reset(); }
+void retro_reset() { gb.reset(); }
 
-static unsigned serialize_size = 0;
-unsigned snes_serialize_size()
+static size_t serialize_size = 0;
+size_t retro_serialize_size()
 {
    return gb.stateSize();
 }
 
-bool snes_serialize(uint8_t *data, unsigned size)
+bool retro_serialize(void *data, size_t size)
 {
    if (serialize_size == 0)
-      serialize_size = snes_serialize_size();
+      serialize_size = retro_serialize_size();
 
    if (size != serialize_size)
       return false;
@@ -126,10 +124,10 @@ bool snes_serialize(uint8_t *data, unsigned size)
    return true;
 }
 
-bool snes_unserialize(const uint8_t *data, unsigned size)
+bool retro_unserialize(const void *data, size_t size)
 {
    if (serialize_size == 0)
-      serialize_size = snes_serialize_size();
+      serialize_size = retro_serialize_size();
 
    if (size != serialize_size)
       return false;
@@ -138,58 +136,35 @@ bool snes_unserialize(const uint8_t *data, unsigned size)
    return true;
 }
 
-void snes_cheat_reset() {}
-void snes_cheat_set(unsigned, bool, const char *) {}
+void retro_cheat_reset() {}
+void retro_cheat_set(unsigned, bool, const char *) {}
 
-bool snes_load_cartridge_normal(
-  const char *, const uint8_t *rom_data, unsigned rom_size
-)
+bool retro_load_game(const struct retro_game_info *info)
 {
-   if (gb.load(rom_data, rom_size))
-      return false;
-
-   return true;
+   return !gb.load(info->data, info->size);
 }
 
-bool snes_load_cartridge_bsx_slotted(
-  const char *, const uint8_t *, unsigned,
-  const char *, const uint8_t *, unsigned
-) { return false; }
+bool retro_load_game_special(unsigned, const struct retro_game_info*, size_t) { return false; }
 
-bool snes_load_cartridge_bsx(
-  const char *, const uint8_t *, unsigned,
-  const char *, const uint8_t *, unsigned
-) { return false; }
+void retro_unload_game() {}
 
-bool snes_load_cartridge_sufami_turbo(
-  const char *, const uint8_t *, unsigned,
-  const char *, const uint8_t *, unsigned,
-  const char *, const uint8_t *, unsigned
-) { return false; }
+unsigned retro_get_region() { return RETRO_REGION_NTSC; }
 
-bool snes_load_cartridge_super_game_boy(
-  const char *, const uint8_t *, unsigned,
-  const char *, const uint8_t *, unsigned
-) { return false; }
-
-void snes_unload_cartridge() {}
-
-bool snes_get_region() { return SNES_REGION_NTSC; }
-uint8_t *snes_get_memory_data(unsigned id)
+void *retro_get_memory_data(unsigned id)
 {
-   if (id == SNES_MEMORY_CARTRIDGE_RAM)
-      return reinterpret_cast<uint8_t*>(gb.savedata_ptr());
-   if (id == SNES_MEMORY_CARTRIDGE_RTC)
-      return reinterpret_cast<uint8_t*>(gb.rtcdata_ptr());
+   if (id == RETRO_MEMORY_SAVE_RAM)
+      return gb.savedata_ptr();
+   if (id == RETRO_MEMORY_RTC)
+      return gb.rtcdata_ptr();
 
    return 0;
 }
 
-unsigned snes_get_memory_size(unsigned id)
+size_t retro_get_memory_size(unsigned id)
 {
-   if (id == SNES_MEMORY_CARTRIDGE_RAM)
+   if (id == RETRO_MEMORY_SAVE_RAM)
       return gb.savedata_size();
-   if (id == SNES_MEMORY_CARTRIDGE_RTC)
+   if (id == RETRO_MEMORY_RTC)
       return gb.rtcdata_size();
 
    return 0;
@@ -200,14 +175,14 @@ static void convert_frame(uint16_t *output, const uint32_t *input)
    for (unsigned y = 0; y < 144; y++)
    {
       const uint32_t *src = input + y * 256;
-      uint16_t *dst = output + y * 1024;
+      uint16_t *dst = output + y * 256;
 
       for (unsigned x = 0; x < 160; x++)
       {
          unsigned color = src[x];
          unsigned out_color = 0;
 
-         // ARGB8888 => XRGB555
+         // ARGB8888 => XRGB555. Should output 32-bit directly if libretro gets ARGB support later on.
          out_color |= (color & 0xf80000) >> (3 + 6);
          out_color |= (color & 0x00f800) >> (3 + 3);
          out_color |= (color & 0x0000f8) >> (3 + 0);
@@ -225,32 +200,22 @@ static void output_audio(const int16_t *samples, unsigned frames)
    int16_t output[2 * 2064];
    std::size_t len = resampler->resample(output, samples, frames);
 
-   if(audio_batch_cb)
-	audio_batch_cb(output, len);
-   else
-   {
-	   for (unsigned i = 0; i < len; i++)
-	   {
-		   int16_t left  = output[2 * i + 0];
-		   int16_t right = output[2 * i + 1];
-		   audio_cb(left, right);
-	   }
-   }
+   if (len)
+      audio_batch_cb(output, len);
 }
 
-void snes_run()
+void retro_run()
 {
    static uint64_t samples_count = 0;
    static uint64_t frames_count = 0;
-   // libsnes uses fixed pitch of 2048 bytes on non-interlaced video.
-   static uint16_t output_video[1024 * 144];
+   static uint16_t output_video[256 * 144];
 
    input_poll_cb();
 
    uint64_t expected_frames = samples_count / 35112;
    if (frames_count < expected_frames) // Detect frame dupes.
    {
-      video_cb(can_dupe ? 0 : output_video, 160, 144);
+      video_cb(can_dupe ? 0 : output_video, 160, 144, 512);
       frames_count++;
       return;
    }
@@ -270,19 +235,13 @@ void snes_run()
       samples = 2064;
    }
 
-   output_audio(sound_buf.i16, samples);
    samples_count += samples;
+   output_audio(sound_buf.i16, samples);
 
    convert_frame(output_video, video_buf);
-   video_cb(output_video, 160, 144);
+   video_cb(output_video, 160, 144, 512);
    frames_count++;
 }
 
-const char *snes_library_id()
-{
-   return "Gambatte";
-}
-
-unsigned snes_library_revision_major() { return 1; }
-unsigned snes_library_revision_minor() { return 3; }
+unsigned retro_api_version() { return RETRO_API_VERSION; }
 
--- libgambatte/libsnes/platform.mk
+++ libgambatte/libsnes/platform.mk
@@ -11,15 +11,15 @@ endif
 endif
 
 ifeq ($(platform), unix)
-   TARGET := libsnes.so
+   TARGET := libretro.so
    fpic := -fPIC
    SHARED := -shared -Wl,--version-script=link.T
 else ifeq ($(platform), osx)
-   TARGET := libsnes.dylib
+   TARGET := libretro.dylib
    fpic := -fPIC
    SHARED := -dynamiclib
 else ifeq ($(platform), wii)
-   TARGET := libsnes.a
+   TARGET := libretro.a
    CXX = powerpc-eabi-g++
    AR = powerpc-eabi-ar
    CXXFLAGS += -mrvl -mcpu=750 -DGEKKO -meabi -mhard-float
