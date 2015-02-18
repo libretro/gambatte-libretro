f0edce424643bfde1f5206892efc57a6ab0f3bcb
twinaphex <libretro@gmail.com>
2015-02-18 05:30:53 +0100

    Upgrade video/ly_counter | lyc_irq | next_m0_time to upstream

--- libgambatte/src/video/ly_counter.cpp
+++ libgambatte/src/video/ly_counter.cpp
@@ -1,77 +1,68 @@
-/***************************************************************************
- *   Copyright (C) 2007 by Sindre Aamås                                    *
- *   aamas@stud.ntnu.no                                                    *
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License version 2 as     *
- *   published by the Free Software Foundation.                            *
- *                                                                         *
- *   This program is distributed in the hope that it will be useful,       *
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
- *   GNU General Public License version 2 for more details.                *
- *                                                                         *
- *   You should have received a copy of the GNU General Public License     *
- *   version 2 along with this program; if not, write to the               *
- *   Free Software Foundation, Inc.,                                       *
- *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
- ***************************************************************************/
+//
+//   Copyright (C) 2007 by sinamas <sinamas at users.sourceforge.net>
+//
+//   This program is free software; you can redistribute it and/or modify
+//   it under the terms of the GNU General Public License version 2 as
+//   published by the Free Software Foundation.
+//
+//   This program is distributed in the hope that it will be useful,
+//   but WITHOUT ANY WARRANTY; without even the implied warranty of
+//   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//   GNU General Public License version 2 for more details.
+//
+//   You should have received a copy of the GNU General Public License
+//   version 2 along with this program; if not, write to the
+//   Free Software Foundation, Inc.,
+//   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+//
+
 #include "ly_counter.h"
 #include "../savestate.h"
 
-namespace gambatte
+namespace gambatte {
+
+LyCounter::LyCounter()
+: time_(0)
+, lineTime_(0)
+, ly_(0)
+, ds_(false)
 {
+	setDoubleSpeed(false);
+	reset(0, 0);
+}
 
-   LyCounter::LyCounter()
-      : time_(0)
-        , lineTime_(0)
-        , ly_(0)
-        , ds(false)
-   {
-      setDoubleSpeed(false);
-      reset(0, 0);
-   }
+void LyCounter::doEvent() {
+	++ly_;
+	if (ly_ == 154)
+		ly_ = 0;
 
-   void LyCounter::doEvent()
-   {
-      ++ly_;
+	time_ = time_ + lineTime_;
+}
 
-      if (ly_ == 154)
-         ly_ = 0;
+unsigned long LyCounter::nextLineCycle(unsigned const lineCycle, unsigned long const cc) const {
+	unsigned long tmp = time_ + (lineCycle << ds_);
+	if (tmp - cc > lineTime_)
+		tmp -= lineTime_;
 
-      time_ = time_ + lineTime_;
-   }
+	return tmp;
+}
 
-   unsigned long LyCounter::nextLineCycle(const unsigned lineCycle, const unsigned long cc) const
-   {
-      unsigned long tmp = time_ + (lineCycle << ds);
+unsigned long LyCounter::nextFrameCycle(unsigned long const frameCycle, unsigned long const cc) const {
+	unsigned long tmp = time_ + (((153U - ly()) * 456U + frameCycle) << ds_);
+	if (tmp - cc > 70224U << ds_)
+		tmp -= 70224U << ds_;
 
-      if (tmp - cc > lineTime_)
-         tmp -= lineTime_;
+	return tmp;
+}
 
-      return tmp;
-   }
+void LyCounter::reset(unsigned long videoCycles, unsigned long lastUpdate) {
+	ly_ = videoCycles / 456;
+	time_ = lastUpdate + ((456 - (videoCycles - ly_ * 456ul)) << isDoubleSpeed());
+}
 
-   unsigned long LyCounter::nextFrameCycle(const unsigned long frameCycle, const unsigned long cc) const
-   {
-      unsigned long tmp = time_ + (((153U - ly()) * 456U + frameCycle) << ds);
-
-      if (tmp - cc > 70224U << ds)
-         tmp -= 70224U << ds;
-
-      return tmp;
-   }
-
-   void LyCounter::reset(const unsigned long videoCycles, const unsigned long lastUpdate)
-   {
-      ly_ = videoCycles / 456;
-      time_ = lastUpdate + ((456 - (videoCycles - ly_ * 456ul)) << isDoubleSpeed());
-   }
-
-   void LyCounter::setDoubleSpeed(const bool ds_in)
-   {
-      ds = ds_in;
-      lineTime_ = 456U << ds_in;
-   }
+void LyCounter::setDoubleSpeed(bool ds) {
+	ds_ = ds;
+	lineTime_ = 456U << ds;
+}
 
 }
