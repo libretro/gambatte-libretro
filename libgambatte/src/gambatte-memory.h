c1d87ecde61558f844a15db85676bd72e62e42a3
twinaphex <libretro@gmail.com>
2015-02-18 02:08:06 +0100

    Update gambatte-memory.h

--- libgambatte/src/gambatte-memory.h
+++ libgambatte/src/gambatte-memory.h
@@ -20,135 +20,152 @@
 #define MEMORY_H
 
 #include "mem/cartridge.h"
-#include "video.h"
-#include "sound.h"
 #include "interrupter.h"
+#include "sound.h"
 #include "tima.h"
+#include "video.h"
 
-namespace gambatte {
-class InputGetter;
-class FilterInfo;
+namespace gambatte
+{
+   class InputGetter;
+   class FilterInfo;
 
-class Memory {
-	unsigned char ioamhram[0x200];
-	unsigned char vram[0x2000 * 2];
-	unsigned char *vrambank;
-	
-	InputGetter *getInput;
-	unsigned long divLastUpdate;
-	unsigned long lastOamDmaUpdate;
-	
-	InterruptRequester intreq;
-	Cartridge cart;
-	Tima tima;
-	LCD display;
-	PSG sound;
-	Interrupter interrupter;
-	
-	unsigned short dmaSource;
-	unsigned short dmaDestination;
-	unsigned char oamDmaPos;
-   unsigned char serialCnt;
-	bool blanklcd;
+   class Memory
+   {
+      public:
+      explicit Memory(const Interrupter &interrupter);
 
-	void updateInput();
-	void decEventCycles(MemEventId eventId, unsigned long dec);
+      void setStatePtrs(SaveState &state);
 
-	void oamDmaInitSetup();
-	void updateOamDma(unsigned long cycleCounter);
-	void startOamDma(unsigned long cycleCounter);
-	void endOamDma(unsigned long cycleCounter);
-	const unsigned char * oamDmaSrcPtr() const;
-	
-	unsigned nontrivial_ff_read(unsigned P, unsigned long cycleCounter);
-	unsigned nontrivial_read(unsigned P, unsigned long cycleCounter);
-	void nontrivial_ff_write(unsigned P, unsigned data, unsigned long cycleCounter);
-	void nontrivial_write(unsigned P, unsigned data, unsigned long cycleCounter);
-	
-   void updateSerial(unsigned long cc);
-	void updateTimaIrq(unsigned long cc);
-	void updateIrqs(unsigned long cc);
-	
-	bool isDoubleSpeed() const { return display.isDoubleSpeed(); }
+#ifdef __LIBRETRO__
+      void *savedata_ptr() { return cart.savedata_ptr(); }
+      unsigned savedata_size() { return cart.savedata_size(); }
+      void *rtcdata_ptr() { return cart.rtcdata_ptr(); }
+      unsigned rtcdata_size() { return cart.rtcdata_size(); }
+#endif
 
-public:
-	explicit Memory(const Interrupter &interrupter);
-	
-	void setStatePtrs(SaveState &state);
-	unsigned long saveState(SaveState &state, unsigned long cc);
-	void loadState(const SaveState &state/*, unsigned long oldCc*/);
-   void *savedata_ptr() { return cart.savedata_ptr(); }
-   unsigned savedata_size() { return cart.savedata_size(); }
-   void *rtcdata_ptr() { return cart.rtcdata_ptr(); }
-   unsigned rtcdata_size() { return cart.rtcdata_size(); }
-	
-	unsigned long stop(unsigned long cycleCounter);
-	bool isCgb() const { return display.isCgb(); }
-	bool ime() const { return intreq.ime(); }
-	bool halted() const { return intreq.halted(); }
-	unsigned long nextEventTime() const { return intreq.minEventTime(); }
-	
-	bool isActive() const { return intreq.eventTime(END) != DISABLED_TIME; }
-	
-	long cyclesSinceBlit(const unsigned long cc) const {
-		return cc < intreq.eventTime(BLIT) ? -1 : static_cast<long>((cc - intreq.eventTime(BLIT)) >> isDoubleSpeed());
-	}
+      void clearCheats() { cart.clearCheats(); }
 
-	void halt() { intreq.halt(); }
-	void ei(unsigned long cycleCounter) { if (!ime()) { intreq.ei(cycleCounter); } }
+      void display_setColorCorrection(bool enable) { display.setColorCorrection(enable); }
+      video_pixel_t display_gbcToRgb32(const unsigned bgr15) { return display.gbcToRgb32(bgr15); }
 
-	void di() { intreq.di(); }
+      void loadState(const SaveState &state/*, unsigned long oldCc*/);
 
-	unsigned ff_read(const unsigned P, const unsigned long cycleCounter) {
-		return P < 0xFF80 ? nontrivial_ff_read(P, cycleCounter) : ioamhram[P - 0xFE00];
-	}
+      unsigned long saveState(SaveState &state, unsigned long cc);
 
-	unsigned read(const unsigned P, const unsigned long cycleCounter) {
-		return cart.rmem(P >> 12) ? cart.rmem(P >> 12)[P] : nontrivial_read(P, cycleCounter);
-	}
+      unsigned long stop(unsigned long cycleCounter);
+      bool isCgb() const { return display.isCgb(); }
+      bool ime() const { return intreq.ime(); }
+      bool halted() const { return intreq.halted(); }
 
-	void write(const unsigned P, const unsigned data, const unsigned long cycleCounter) {
-		if (cart.wmem(P >> 12)) {
-			cart.wmem(P >> 12)[P] = data;
-		} else
-			nontrivial_write(P, data, cycleCounter);
-	}
-	
-	void ff_write(const unsigned P, const unsigned data, const unsigned long cycleCounter) {
-		if (P - 0xFF80u < 0x7Fu) {
-			ioamhram[P - 0xFE00] = data;
-		} else
-			nontrivial_ff_write(P, data, cycleCounter);
-	}
+      unsigned long nextEventTime() const { return intreq.minEventTime(); }
 
-	unsigned long event(unsigned long cycleCounter);
-	unsigned long resetCounters(unsigned long cycleCounter);
+      bool isActive() const { return intreq.eventTime(END) != DISABLED_TIME; }
 
-	bool loadROM(const std::string &romfile, bool forceDmg, bool multiCartCompat);
-	bool loadROM(const void *romdata, unsigned romsize, bool forceDmg, bool multiCartCompat);
-	void setSaveDir(const std::string &dir) { cart.setSaveDir(dir); }
+      long cyclesSinceBlit(const unsigned long cc) const
+      {
+         if (cc < intreq.eventTime(BLIT))
+            return -1;
+         return (cc - intreq.eventTime(BLIT)) >> isDoubleSpeed();
+      }
 
-	void setInputGetter(InputGetter *getInput) {
-		this->getInput = getInput;
-	}
+      void halt() { intreq.halt(); }
+      void ei(unsigned long cycleCounter) { if (!ime()) { intreq.ei(cycleCounter); } }
+      void di() { intreq.di(); }
 
-	void setEndtime(unsigned long cc, unsigned long inc);
-	
-	void setSoundBuffer(uint_least32_t *const buf) { sound.setBuffer(buf); }
-	unsigned fillSoundBuffer(unsigned long cc);
-	
-	void setVideoBuffer(video_pixel_t *const videoBuf, const int pitch) {
-		display.setVideoBuffer(videoBuf, pitch);
-	}
-	
-	void setDmgPaletteColor(unsigned palNum, unsigned colorNum, unsigned long rgb32);
-   void setGameGenie(const std::string &codes) { cart.setGameGenie(codes); }
-   void clearCheats() { cart.clearCheats(); }
-   void setGameShark(const std::string &codes) { interrupter.setGameShark(codes); }
-	
-	void display_setColorCorrection(bool enable) { display.setColorCorrection(enable); }
-	video_pixel_t display_gbcToRgb32(const unsigned bgr15) { return display.gbcToRgb32(bgr15); }
-};
+      unsigned ff_read(const unsigned P, const unsigned long cycleCounter)
+      {
+         return P < 0xFF80 ? nontrivial_ff_read(P, cycleCounter) : ioamhram[P - 0xFE00];
+      }
+
+      unsigned read(const unsigned P, const unsigned long cycleCounter)
+      {
+         return cart.rmem(P >> 12) ? cart.rmem(P >> 12)[P] : nontrivial_read(P, cycleCounter);
+      }
+
+      void write(const unsigned P, const unsigned data, const unsigned long cycleCounter)
+      {
+         if (cart.wmem(P >> 12))
+            cart.wmem(P >> 12)[P] = data;
+         else
+            nontrivial_write(P, data, cycleCounter);
+      }
+
+      void ff_write(const unsigned P, const unsigned data, const unsigned long cycleCounter)
+      {
+         if (P - 0xFF80u < 0x7Fu)
+            ioamhram[P - 0xFE00] = data;
+         else
+            nontrivial_ff_write(P, data, cycleCounter);
+      }
+
+      unsigned long event(unsigned long cycleCounter);
+      unsigned long resetCounters(unsigned long cycleCounter);
+
+      bool loadROM(const std::string &romfile, bool forceDmg, bool multiCartCompat);
+      bool loadROM(const void *romdata, unsigned romsize, bool forceDmg, bool multiCartCompat);
+
+      void setSaveDir(const std::string &dir)
+      {
+         cart.setSaveDir(dir);
+      }
+
+      void setInputGetter(InputGetter *getInput)
+      {
+         this->getInput = getInput;
+      }
+
+      void setEndtime(unsigned long cc, unsigned long inc);
+      void setSoundBuffer(uint_least32_t *const buf) { sound.setBuffer(buf); }
+      unsigned fillSoundBuffer(unsigned long cc);
+
+      void setVideoBuffer(video_pixel_t *const videoBuf, const int pitch)
+      {
+         display.setVideoBuffer(videoBuf, pitch);
+      }
+
+      void setDmgPaletteColor(unsigned palNum, unsigned colorNum, unsigned long rgb32);
+
+      void setGameGenie(const std::string &codes) { cart.setGameGenie(codes); }
+      void setGameShark(const std::string &codes) { interrupter.setGameShark(codes); }
+      void updateInput();
+
+      private:
+      Cartridge cart;
+      unsigned char ioamhram[0x200];
+      InputGetter *getInput;
+      unsigned long divLastUpdate;
+      unsigned long lastOamDmaUpdate;
+      InterruptRequester intreq;
+      Tima tima;
+      LCD display;
+      PSG sound;
+      Interrupter interrupter;
+      unsigned short dmaSource;
+      unsigned short dmaDestination;
+      unsigned char oamDmaPos;
+      unsigned char serialCnt;
+      bool blanklcd;
+
+      unsigned char vram[0x2000 * 2];
+      unsigned char *vrambank;
+
+      void decEventCycles(MemEventId eventId, unsigned long dec);
+      void oamDmaInitSetup();
+      void updateOamDma(unsigned long cycleCounter);
+      void startOamDma(unsigned long cycleCounter);
+      void endOamDma(unsigned long cycleCounter);
+      const unsigned char * oamDmaSrcPtr() const;
+      unsigned nontrivial_ff_read(unsigned P, unsigned long cycleCounter);
+      unsigned nontrivial_read(unsigned P, unsigned long cycleCounter);
+      void nontrivial_ff_write(unsigned P, unsigned data, unsigned long cycleCounter);
+      void nontrivial_write(unsigned P, unsigned data, unsigned long cycleCounter);
+      void updateSerial(unsigned long cc);
+      void updateTimaIrq(unsigned long cc);
+      void updateIrqs(unsigned long cc);
+      bool isDoubleSpeed() const { return display.isDoubleSpeed(); }
+
+   };
 
 }
 
