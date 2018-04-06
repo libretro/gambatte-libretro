b9c9d394d164c107f5dd2222f854b29f2f7549b2
Aaron Kling <webgeek1234@gmail.com>
2018-04-06 14:29:30 -0500

    Refactor android jni makefiles

--- libgambatte/libretro/jni/Android.mk
+++ libgambatte/libretro/jni/Android.mk
@@ -1,36 +1,23 @@
 LOCAL_PATH := $(call my-dir)
 
-include $(CLEAR_VARS)
+ROOT_DIR     := $(LOCAL_PATH)/../../..
+CORE_DIR     := $(ROOT_DIR)/libgambatte/src
+LIBRETRO_DIR := $(ROOT_DIR)/libgambatte/libretro
 
+HAVE_NETWORK := 1
 
-HAVE_NETWORK = 1
-LOCAL_MODULE    := libretro
+include $(ROOT_DIR)/Makefile.common
 
-ifeq ($(TARGET_ARCH),arm)
-LOCAL_CXXFLAGS += -DANDROID_ARM
-LOCAL_ARM_MODE := arm
-endif
-
-ifeq ($(TARGET_ARCH),x86)
-LOCAL_CXXFLAGS +=  -DANDROID_X86
-endif
-
-ifeq ($(TARGET_ARCH),mips)
-LOCAL_CXXFLAGS += -DANDROID_MIPS
-endif
-
-CORE_DIR := ../../src
-
-include ../../../Makefile.common
-
-LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C)
-LOCAL_CXXFLAGS += -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DVIDEO_RGB565
+COREFLAGS := -DINLINE=inline -DHAVE_STDINT_H -DHAVE_INTTYPES_H -D__LIBRETRO__ -DVIDEO_RGB565
 
 ifeq ($(HAVE_NETWORK),1)
-LOCAL_CXXFLAGS += -DHAVE_NETWORK
+  COREFLAGS += -DHAVE_NETWORK
 endif
-LOCAL_C_INCLUDES = $(CORE_DIR) $(CORE_DIR)/../include $(CORE_DIR)/../libretro-common/include $(CORE_DIR)/../../common $(CORE_DIR)/../../common/resample $(CORE_DIR)/../libretro
 
-LOCAL_LDLIBS := -latomic
-
+include $(CLEAR_VARS)
+LOCAL_MODULE    := retro
+LOCAL_SRC_FILES := $(SOURCES_CXX) $(SOURCES_C)
+LOCAL_CXXFLAGS  := $(COREFLAGS) $(INCFLAGS)
+LOCAL_CFLAGS    := $(INCFLAGS)
+LOCAL_LDFLAGS   := -Wl,-version-script=$(LIBRETRO_DIR)/link.T
 include $(BUILD_SHARED_LIBRARY)
