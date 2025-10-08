55db290c299e045509261d8f74275e068e62c0d7
Alex <alex_carbon@emulationonline.com>
2025-10-08 17:48:00 -0500

    wip - impl frame/framebuffer.
    frame hangs indefinitely.

--- libgb.cpp
+++ libgb.cpp
@@ -2,6 +2,11 @@
 #include "libgambatte/include/gambatte.h"
 
 gambatte::GB *gameboy_ = nullptr;
+uint32_t fbuffer[160 * 144];
+const int FB_PITCH_PX = 160;
+const size_t SOUND_SAMPLES_PER_RUN = 2064;
+const size_t SOUND_BUFF_SIZE = (SOUND_SAMPLES_PER_RUN + 2064);
+gambatte::uint_least32_t sbuffer[SOUND_BUFF_SIZE];
 
 extern "C" 
 __attribute__((visibility("default")))
@@ -22,12 +27,16 @@ void init(const uint8_t* data, size_t len) {
 extern "C" 
 __attribute__((visibility("default")))
 const uint8_t *framebuffer() {
-    return 0;
+    return (const uint8_t*)fbuffer;
 }
 
 extern "C"
 __attribute__((visibility("default")))
 void frame() {
+    if (gameboy_ == nullptr) return;
+    unsigned samples;
+    while (-1 == gameboy_->runFor((long unsigned int*)fbuffer, FB_PITCH_PX,
+                /*soundbuf*/ sbuffer, /*soundBufSize*/SOUND_BUFF_SIZE, samples)) {}
 }
 
 extern "C"
